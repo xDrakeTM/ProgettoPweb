273327ebdb5284112306f47f2115a2415ecd402e
xDrakeTM <96006415+xDrakeTM@users.noreply.github.com>
2024-12-22 22:27:28 +0100

    controllo sessione

--- progetto_pweb/index.php
+++ progetto_pweb/index.php
@@ -1,5 +1,7 @@
 <?php
-    session_start();
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
 
     if ($_SESSION["user_tipo"] == "utente") {
         header("Location: php/homeUtente.php");
