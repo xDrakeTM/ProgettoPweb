901f64176078ce933709f474dbd96372a612eb24
xDrakeTM <96006415+xDrakeTM@users.noreply.github.com>
2024-12-24 17:33:23 +0100

    indentazione

--- progetto_pweb/php/homeUtente.php
+++ progetto_pweb/php/homeUtente.php
@@ -6,54 +6,54 @@
     menuUtente();
 ?>
 
-        <main>
-            <section class="welcome-section">
-                <?php
-                switch ($_SESSION["user_genere"]) {
-                    case 'M':
-                        echo "<h2>Benvenuto, " . htmlspecialchars($_SESSION['user_nome']) . "!</h2>"; 
-                        break;
+<main>
+    <section class="welcome-section">
+        <?php
+        switch ($_SESSION["user_genere"]) {
+            case 'M':
+                echo "<h2>Benvenuto, " . htmlspecialchars($_SESSION['user_nome']) . "!</h2>"; 
+                break;
 
-                    case 'F':
-                        echo "<h2>Benvenuta, " . htmlspecialchars($_SESSION['user_nome']) . "!</h2>"; 
-                        break;
+            case 'F':
+                echo "<h2>Benvenuta, " . htmlspecialchars($_SESSION['user_nome']) . "!</h2>"; 
+                break;
 
-                    case 'A':
-                        echo "<h2>Benvenut*, " . htmlspecialchars($_SESSION['user_nome']) . "!</h2>"; 
-                        break;
-                }
-                ?>
-                <p>Esplora le nostre funzionalità e prenota il tuo allenamento!</p>
-            </section>
+            case 'A':
+                echo "<h2>Benvenut*, " . htmlspecialchars($_SESSION['user_nome']) . "!</h2>"; 
+                break;
+        }
+        ?>
+        <p>Esplora le nostre funzionalità e prenota il tuo allenamento!</p>
+    </section>
 
-            <section class="features-section">
-                <div class="feature-box">
-                    <h3>Prenota il tuo Allenamento</h3>
-                    <p>Prenota il tuo allenamento tranquillamente online!</p>
-                    <a href="prenotazione.php" class="button">Prenota</a>
-                </div>
-                <div class="feature-box">
-                    <h3>Controlla i tuoi Appuntamenti</h3>
-                    <p>Visualizza lo stato del tuo appuntamento!</p>
-                    <a href="allenamenti.php" class="button">Vai</a>
-                </div>
-                <div class="feature-box">
-                    <h3>I tuoi Obiettivi</h3>
-                    <p>Compila i tuoi obiettivi raggiunti in allenamento!</p>
-                    <a href="obiettivi.php" class="button">Vai</a>
-                </div>
-                <div class="feature-box">
-                    <h3>Il tuo Profilo</h3>
-                    <p>Gestisci le tue informazioni personali.</p>
-                    <a href="profiloUtente.php" class="button">Vai al Profilo</a>
-                </div>
-                <div class="feature-box">
-                    <h3>Le tue Statistiche</h3>
-                    <p>Vedi le tue statistiche.</p>
-                    <a href="statistiche.php" class="button">Vai</a>
-                </div>
-            </section>
-        </main>
+    <section class="features-section">
+        <div class="feature-box">
+            <h3>Prenota il tuo Allenamento</h3>
+            <p>Prenota il tuo allenamento tranquillamente online!</p>
+            <a href="prenotazione.php" class="button">Prenota</a>
+        </div>
+        <div class="feature-box">
+            <h3>Controlla i tuoi Appuntamenti</h3>
+            <p>Visualizza lo stato del tuo appuntamento!</p>
+            <a href="allenamenti.php" class="button">Vai</a>
+        </div>
+        <div class="feature-box">
+            <h3>I tuoi Obiettivi</h3>
+            <p>Compila i tuoi obiettivi raggiunti in allenamento!</p>
+            <a href="obiettivi.php" class="button">Vai</a>
+        </div>
+        <div class="feature-box">
+            <h3>Il tuo Profilo</h3>
+            <p>Gestisci le tue informazioni personali.</p>
+            <a href="profiloUtente.php" class="button">Vai al Profilo</a>
+        </div>
+        <div class="feature-box">
+            <h3>Le tue Statistiche</h3>
+            <p>Vedi le tue statistiche.</p>
+            <a href="statistiche.php" class="button">Vai</a>
+        </div>
+    </section>
+</main>
 <?php
     _footer();
 ?>
--- progetto_pweb/php/profiloPT.php
+++ progetto_pweb/php/profiloPT.php
@@ -22,56 +22,56 @@
     <script src="../js/profilo.js"></script>', $sql);
     menuPT();
 ?>
-        <main>
-            <section class="welcome-section">
-                <h1>Modifica il tuo Profilo</h1>
+<main>
+    <section class="welcome-section">
+        <h1>Modifica il tuo Profilo</h1>
 
-                <form id="updateProfileForm" onsubmit="validaModificaProfiloPT(event)">
-                    <label for="nome">Nome:</label>
-                    <input type="text" id="nome" name="nome" value="<?php echo htmlspecialchars($user['nome']); ?>" readonly disabled>
+        <form id="updateProfileForm" onsubmit="validaModificaProfiloPT(event)">
+            <label for="nome">Nome:</label>
+            <input type="text" id="nome" name="nome" value="<?php echo htmlspecialchars($user['nome']); ?>" readonly disabled>
 
-                    <label for="cognome">Cognome:</label>
-                    <input type="text" id="cognome" name="cognome" value="<?php echo htmlspecialchars($user['cognome']); ?>" readonly disabled>
+            <label for="cognome">Cognome:</label>
+            <input type="text" id="cognome" name="cognome" value="<?php echo htmlspecialchars($user['cognome']); ?>" readonly disabled>
 
-                    <label for="email">Email:</label>
-                    <input type="email" id="email" name="email" value="<?php echo htmlspecialchars($user['email']); ?>" required>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" value="<?php echo htmlspecialchars($user['email']); ?>" required>
 
-                    <label for="data_nascita">Data di Nascita:</label>
-                    <input type="date" id="data_nascita" name="data_nascita" value="<?php echo $user['data_nascita']; ?>" readonly disabled>
+            <label for="data_nascita">Data di Nascita:</label>
+            <input type="date" id="data_nascita" name="data_nascita" value="<?php echo $user['data_nascita']; ?>" readonly disabled>
 
-                    <label for="genere">Genere:</label>
-                    <input type="text" id="genere" name="genere" value="<?php 
-                        switch ($_SESSION["user_genere"]) {
-                            case 'M':
-                                echo "Maschio"; 
-                                break;
+            <label for="genere">Genere:</label>
+            <input type="text" id="genere" name="genere" value="<?php 
+                switch ($_SESSION["user_genere"]) {
+                    case 'M':
+                        echo "Maschio"; 
+                        break;
 
-                            case 'F':
-                                echo "Femmina"; 
-                                break;
+                    case 'F':
+                        echo "Femmina"; 
+                        break;
 
-                            case 'A':
-                                echo "Altro"; 
-                                break;
-                        }
-                    ?>" readonly disabled>
+                    case 'A':
+                        echo "Altro"; 
+                        break;
+                }
+            ?>" readonly disabled>
 
-                    <label for="cellulare">Cellulare:</label>
-                    <input type="tel" id="cellulare" name="cellulare" value="<?php echo $user['cellulare']; ?>" required>
+            <label for="cellulare">Cellulare:</label>
+            <input type="tel" id="cellulare" name="cellulare" value="<?php echo $user['cellulare']; ?>" required>
 
-                    <label for="curriculum">Curriculum (PDF, DOC, DOCX):</label>
-                    <input type="file" id="curriculum" name="curriculum" accept=".pdf,.doc,.docx">
-                    <p style="margin-bottom: 20px;">Curriculum attuale: <a href="../curriculum/<?php echo htmlspecialchars($user['curriculum']); ?>">Visualizza</a></p>
+            <label for="curriculum">Curriculum (PDF, DOC, DOCX):</label>
+            <input type="file" id="curriculum" name="curriculum" accept=".pdf,.doc,.docx">
+            <p style="margin-bottom: 20px;">Curriculum attuale: <a href="../curriculum/<?php echo htmlspecialchars($user['curriculum']); ?>">Visualizza</a></p>
 
-                    <button type="submit">Aggiorna Profilo</button>
-                </form>
+            <button type="submit">Aggiorna Profilo</button>
+        </form>
 
-                <button id="modifyPasswordButton" style="background-color: orange;" onclick="window.location.href='../html/modificaPassword.html'">Modifica Password</button>
-                <button id="deleteAccountButton" style="background-color: red;" onclick="eliminaAccount()">Elimina Account</button>
+        <button id="modifyPasswordButton" style="background-color: orange;" onclick="window.location.href='../html/modificaPassword.html'">Modifica Password</button>
+        <button id="deleteAccountButton" style="background-color: red;" onclick="eliminaAccount()">Elimina Account</button>
 
-                <div id="status"></div>
-            </section>
-        </main>
+        <div id="status"></div>
+    </section>
+</main>
 <?php
     _footer();
 ?>
