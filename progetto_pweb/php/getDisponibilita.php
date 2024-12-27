86cec1f917c535985fca12179fb4cbe988d90b8e
xDrakeTM <96006415+xDrakeTM@users.noreply.github.com>
2024-12-27 17:01:05 +0100

    correzioni

--- progetto_pweb/php/getDisponibilita.php
+++ progetto_pweb/php/getDisponibilita.php
@@ -20,9 +20,16 @@
 
     if (isset($_GET['id_pt'])) {
         $id_pt = $_GET['id_pt'];
+        $user_id = $_SESSION["user_id"];
 
-        $stmt = $conn->prepare("SELECT data, ora FROM appuntamento WHERE personal_trainer_id = ? AND stato = 'confermato'");
-        $stmt->bind_param("i", $id_pt);
+        // Recupera le disponibilità del personal trainer e gli appuntamenti dell'utente
+        $stmt = $conn->prepare("
+            SELECT data, ora 
+            FROM appuntamento 
+            WHERE (personal_trainer_id = ? AND stato = 'confermato') 
+            OR (utente_id = ? AND stato = 'prenotato')
+        ");
+        $stmt->bind_param("ii", $id_pt, $user_id);
         $stmt->execute();
         $result = $stmt->get_result();
 
